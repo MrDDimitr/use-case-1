c1da8870a7b5bed00ba0cd579658e96fb51b6ad8
MrDDimitr <ddimitr@softserveinc.com>
2023-08-28 11:42:25 +0300

    UC#1 Create sort function by name

--- use-case-1/src/components/SimpleForm.js
+++ use-case-1/src/components/SimpleForm.js
@@ -3,13 +3,17 @@ import axios from "axios"
 
 const SimpleForm = () => {
     const [countries, setCountries] = useState([])
-    const setFilteredCountries = useState([])[1]
+    const [filteredCountries, setFilteredCountries] = useState([])
     const [countryName, setCountryName] = useState("")
     const [population, setPopulation] = useState(0)
+    const [sort, setSort] = useState("")
 
     const getCountries = () => {
         axios.get("https://restcountries.com/v3.1/all")
-            .then((resp) => setCountries(resp.data))
+            .then((resp) => {
+                setCountries(resp.data)
+                setFilteredCountries(resp.data)
+            })
     }
 
     useEffect(() => {
@@ -31,6 +35,16 @@ const SimpleForm = () => {
         setFilteredCountries(filtered)
     }
 
+    const sortByName = (sortValue) => {
+        if (sortValue === "ascend") {
+            const sorted = filteredCountries.sort((a, b) => a.name.common.localeCompare(b.name.common))
+            setFilteredCountries(sorted)
+        } else if (sortValue === "descend") {
+            const sorted = filteredCountries.sort((a, b) => b.name.common.localeCompare(a.name.common))
+            setFilteredCountries(sorted)
+        }
+    }
+
     const handleOnCountryNameChange = (e) => {
         const { value } = e.target
         setCountryName(value)
@@ -42,6 +56,12 @@ const SimpleForm = () => {
         setPopulation(value)
         const numberValue = parseInt(value)
         filterByPopulation(numberValue)
+    }
+
+    const handleOnSortChange = (e) => {
+        const { value } = e.target
+        setSort(value)
+        sortByName(value)
     }
 
     return (
@@ -63,8 +83,12 @@ const SimpleForm = () => {
                 />
             </section>
             <section>
-                <label htmlFor="">Name</label>
-                <input type="text" />
+                <label htmlFor="">Sort</label>
+                <input 
+                    type="text"
+                    value={sort}
+                    onChange={handleOnSortChange}
+                />
             </section>
             <section>
                 <label htmlFor="">Name</label>
